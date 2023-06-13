5fe03475a874bc138025f4f9f070de9f34173cf7
Mattias <mattiaswelamsson@gmail.com>
2023-06-14 07:15:25 +0900

    add render template

--- users/views.py
+++ users/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+
 from django.http import HttpResponse
+from django.template import loader
+
 # Create your views here.
 
+
 def users(request):
-    return HttpResponse("Hi there!")
+    # return HttpResponse("Hi th_ere!")
+    template = loader.get_template("usersindex.html")
+    return HttpResponse(template.render())
+
 
 # Create an actual action, rendering a page.

17c77142580ccc95f3c41a3ebf5d9865b75a7bb4
Mattias <mattiaswelamsson@gmail.com>
2023-06-14 08:43:16 +0900

    added serializer

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import User
+from .serializers import UserSerializer
 
 # Create your views here.
 

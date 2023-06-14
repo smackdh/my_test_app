b375554ecd4eaa56ce47bb01a73b90933baac509
Mattias <mattiaswelamsson@gmail.com>
2023-06-14 15:38:06 +0900

    update django

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import User
 
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("firstname", "lastname")
+
+
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User, UserAdmin)

af17060577cf64edfa4f874659e7e518f9d815bd
Mattias <mattiaswelamsson@gmail.com>
2023-06-14 10:51:16 +0900

    change display for admin

--- users/models.py
+++ users/models.py
@@ -6,3 +6,6 @@ class User(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
     age = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
